269516cf580249230fc290d4bec5799bb79e9231
Angie Goza <angielg9888@gmail.com>
2018-10-10 17:32:43 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,4 @@
-def turn_count(board)
-  counter = 0
-  board.each do |index|
-    if index == "X" || index == "O"
-      counter += 1
-    end
-  end
-  return counter
-  end
+
   
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
